2b70b39d8094a353f41222c970b9dd6dc4e5674a
caiweill <80924478+caiweill@users.noreply.github.com>
2025-01-25 23:02:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,9 @@
-####
+# ---------------------------------------------------------------
+## OpenClash
+# git clone -b v0.45.141-beta --depth=1 https://github.com/vernesong/openclash.git OpenClash
+git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
+rm -rf feeds/luci/applications/luci-app-openclash
+mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
+# ---------------------------------------------------------------
+
+
